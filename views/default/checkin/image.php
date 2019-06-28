4d6badd2581b6af75585593f9bfa70a6c302b63d
Per Jensen <admin@perjensen-online.dk>
2019-06-28 17:47:21 +0200

    feature(views): opens checkin image in lightbox

--- views/default/checkin/image.php
+++ views/default/checkin/image.php
@@ -16,4 +16,11 @@ $img = elgg_format_element('img', [
 	'src' => $file->getIconURL('checkin_cover'),
 ]);
 
-echo elgg_format_element('div', ['class' => 'checkin-photo'], $img);
+$a = elgg_format_element([
+	'#tag_name' => 'a',
+	'#text' => $img,
+	'href' => $file->getIconURL('checkin_cover'),
+	'class' => 'elgg-lightbox-photo',
+]);
+
+echo elgg_format_element('div', ['class' => 'checkin-photo'], $a);
